49bd037b2e6991d568833b5d9c951dc5e4d66fcd
SantoshN7 <santoshnachan1998@gmail.com>
2020-09-12 14:44:49 +0530

    init commit 2

--- pullmerge.sh
+++ pullmerge.sh
@@ -3,8 +3,8 @@
 inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 if [ "$inside_git_repo" ]; then
 
-    CHANGED="$(git status --porcelain)"
-    if [ -z "${CHANGED}" ]; then
+    is_git_repo_clean="$(git status --porcelain)"
+    if [ -z "${is_git_repo_clean}" ]; then
         echo "Branch is clean.";
     else
         echo "Branch is not clean.";
